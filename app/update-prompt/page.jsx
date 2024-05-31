6053ba58ed295ca245a283febd023b982c0eec5e
HIMANSHU6001 <hk9797592893@gmail.com>
2024-05-31 17:20:35 +0530

    if

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -14,7 +14,7 @@ const UpdatePrompt = () => {
 
   useEffect(() => {
     console.log(router.params.id);
-    if (router.params.id) {
+    if (router.params && router.params.id) {
       setPromptId(router.params.id);
     }
   }, [router.params]);
